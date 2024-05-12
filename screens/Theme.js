f4475fd14cd1deeacdd8d3bebec4c6ea9ea756a6
dl-little <little.douglas22@gmail.com>
2024-05-12 01:14:47 -0400

    Base Styles

--- screens/Theme.js
+++ screens/Theme.js
@@ -3,10 +3,21 @@ import { StyleSheet } from 'react-native';
 const Theme = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#AEAEAE',
-		alignItems: 'center',
+		backgroundColor: '#E8E8E8',
 		justifyContent: 'center',
+		padding: 20,
 	},
+	text: {
+		color: '#1E1E1E',
+	},
+	textInput: {
+		marginVertical: 4,
+		height: 50,
+		borderWidth: 1,
+		borderRadius: 4,
+		padding: 10,
+		backgroundColor: '#FFFFFF'
+	}
 });
 
 export default Theme;
