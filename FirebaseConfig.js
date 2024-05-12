f527b1d875e25b06f72585de701939fdaeb7cb10
dl-little <little.douglas22@gmail.com>
2024-05-12 01:15:02 -0400

    Install firebase

---
+++ FirebaseConfig.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const apiKey = process.env.EXPO_PUBLIC_API_KEY;
+const authDomain = process.env.EXPO_PUBLIC_AUTH_DOMAIN;
+const projectId = process.env.EXPO_PUBLIC_PROJECT_ID;
+const storageBucket = process.env.EXPO_PUBLIC_STORAGE_BUCKET;
+const messagingSenderId = process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID;
+const appId = process.env.EXPO_PUBLIC_APP_ID;
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId
+};
+
+// Initialize Firebase
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
